f7c6aff2469d275f7e96e20bfe91e1c822061fa2
Vensah <83892875+vensah-dev@users.noreply.github.com>
2024-02-14 00:03:10 +0800

    wwww

---
+++ script.js
@@ -0,0 +1,26 @@
+//display the tab
+function displayContent(evt, divID) {
+    var i, tabcontent, tabButtons;
+
+    //get all the tab contents
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].setAttribute("hidden", "hidden")
+    }  
+    //get all the tab buttons
+    tabButtons = document.getElementsByClassName("tabButton");
+    for (i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+    }
+  
+    //set the selected tab to show
+    document.getElementById(divID).removeAttribute("hidden")
+
+    //get the div or whatever that called this function to have the active class to make it look like it was clicked :>
+    evt.currentTarget.className += " active";
+  }
+
+document.getElementById("default").click();
+
+
+
